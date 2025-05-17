2724ef7eff5959444affc51753117b335472c217
rahim709 <abdurrahimer2004@gmail.com>
2025-05-17 10:19:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const checkWin = ()=> {
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
             gameover.play();
-            document.querySelector(".line").style.width = "20vw";
+            document.querySelector(".line").style.width = "0vw";
             document.querySelector(".line").style.transform = `translate(${e[3]}vw,${e[4]}vw) rotate(${e[5]}deg)`;
         }
     });
