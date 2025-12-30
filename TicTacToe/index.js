514e60aced23672792a849298cb3c12f3abd49e9
rahim709 <abdurrahimer2004@gmail.com>
2025-12-30 20:40:18 +0530

    fix game logic

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,4 +1,4 @@
-// Initializing Game Variables
+// Game initialization
 console.log("Tic Tac Toe Started");
 let turn = "X";
 let isgameover = false;
@@ -8,20 +8,16 @@ const changeTurn = () => {
     return turn === "X" ? "0" : "X";
 }
 
-// Function to check for a win
+// Function to check for a win or a draw
 const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]             
     ];
 
+    // 1. Check for Winner
     wins.forEach(e => {
         if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && 
             (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && 
@@ -29,35 +25,46 @@ const checkWin = () => {
             
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Has Won! 🎉";
             isgameover = true;
-            
-            // Show celebration GIF
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
         }
-    })
+    });
+
+    // 2. Check for Draw (only if no one has won yet)
+    if (!isgameover) {
+        let allFilled = true;
+        Array.from(boxtext).forEach(element => {
+            if (element.innerText === '') {
+                allFilled = false;
+            }
+        });
+
+        if (allFilled) {
+            document.querySelector('.info').innerText = "It's a Draw! 🤝";
+            isgameover = true;
+        }
+    }
 }
 
-// Game Logic - Adding click listeners to boxes
+// Click listener logic
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext');
     element.addEventListener('click', () => {
         if (boxtext.innerText === '' && !isgameover) {
             boxtext.innerText = turn;
-            
-            // Set different colors for X and 0
             boxtext.style.color = (turn === "X") ? "#bb86fc" : "#03dac6";
             
-            checkWin();
+            checkWin(); // This now checks for both Win and Draw
             
             if (!isgameover) {
                 turn = changeTurn();
-                document.getElementsByClassName('info')[0].innerText = "Turn for " + turn;
+                document.querySelector('.info').innerText = "Turn for " + turn;
             }
         }
     })
 })
 
-// Reset Button functionality
+// Reset button logic
 document.getElementById('reset').addEventListener('click', () => {
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
@@ -65,6 +72,6 @@ document.getElementById('reset').addEventListener('click', () => {
     });
     turn = "X";
     isgameover = false;
-    document.getElementsByClassName('info')[0].innerText = "Turn for " + turn;
+    document.querySelector('.info').innerText = "Turn for " + turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
 })
